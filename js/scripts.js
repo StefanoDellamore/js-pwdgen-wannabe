7da0779c4968bfcbb4db86c2867ace4a911f1e4e
StefanDellamore <dellamoreste.info@gmail.com>
2023-12-01 01:32:05 +0100

    Esecuzione esercizio

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+// variabile-1
+let textName = prompt('Inserire il proprio nome:');
+console.log("nome inserito dall'utente:", textName);
+
+
+console.log(`
+    nome utente:
+   ${textName} 
+`)
+
+// variabile-2
+
+let textSurname = prompt('Inserire il proprio Cognome:');
+console.log("cogome inserito dall'utente:", textSurname);
+
+
+console.log(`
+    cognome utente:
+   ${textSurname} 
+`)
+
+// variabile-3
+
+let textColor = prompt('Inserire il proprio colore preferito:');
+console.log("colore inserito dall'utente:", textColor);
+
+
+console.log(`
+    colore utente:
+   ${textColor} 
+`)
+
+
+// variabile-4
+
+let datiUtente = textName + ' ' + textSurname + ' ' + textColor;
+console.log(`
+    dati utente:
+   ${datiUtente} 
+`)
+
+// variabile-5
+
+let datiUtenteFinale = datiUtente + ' ' + ('114')
+document.writeln (datiUtenteFinale)
